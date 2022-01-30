2a99291d10979c4a8461fd9e7b811ad2c3e17d57
Stephen Long <longstephen444@gmail.com>
2022-01-29 18:41:22 -0600

    hopefully fixed some broken linking

--- static/js/plots.js
+++ static/js/plots.js
@@ -18,7 +18,7 @@ function optionChanged(newSample) {
 };
 
 function buildMetadata(sample) {
-    d3.json("samples.json").then((data) => {
+    d3.json("static/data/samples.json").then((data) => {
       var metadata = data.metadata;
       var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
       var result = resultArray[0];
